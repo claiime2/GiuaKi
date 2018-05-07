ee5a600522611da247ce2745c2b97bbf01213002
Trần Gia bảo Ngọc <trangiabaongoc1996@gmail.com>
2018-05-07 16:36:50 +0700

    ngoc han

--- QuanliSach-N14DCAT082/app/src/main/java/com/example/user/quanlisach/View/SachInterface.java
+++
@@ -1,7 +0,0 @@
-package com.example.user.quanlisach.View;
-
-import com.example.user.quanlisach.Model.SachModel;
-
-public interface SachInterface {
-    void getSachModel(SachModel sachModel);
-}
--- QuanliSach-N14DCAT082/app/src/main/java/com/example/user/quanlisach/View/VitriInterface.java
+++
@@ -1,7 +0,0 @@
-package com.example.user.quanlisach.View;
-
-import com.example.user.quanlisach.Model.VitriModel;
-
-public interface VitriInterface {
-    void getVitriModel(VitriModel vitriModel);
-}
